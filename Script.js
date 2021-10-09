d3f9938678dd524551bb117ac7f502558751d5f7
Pramit Majumder <rhijusudipta@gmail.com>
2021-10-09 22:42:17 +0530

    Changed Icon

--- Script.js
+++ Script.js
@@ -1,9 +1,26 @@
 function dark(){
     document.body.style.backgroundColor = "#0e122b";
     document.body.style.color = "white";
+    var object = document.getElementById('dark');
+    dark.backgroundColor = "0e122b";
+    dark.color = "white";
+    localStorage.setItem('Appearance',"Dark");
+    console.log('Dark');
 }
 function light(){
     document.body.style.backgroundColor = "white";
     document.body.style.color = "#0e122b"; 
     console.log("yes");
+    localStorage.setItem('Appearance',"Light");
+    console.log('Light');
+}
+
+var mode = localStorage.getItem('Appearance');
+console.log(mode); 
+
+if (mode === "Light"){
+    light()
+}
+else{
+    dark()
 }
